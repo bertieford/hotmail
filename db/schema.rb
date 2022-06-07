fbb2e49b54248444dba75f01768c1c239ad203e6
Robert Ford <robertford88@gmail.com>
2022-06-07 14:52:34 +0100

    migration of fields to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_112208) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_134141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.string "manufacturer"
+    t.string "price"
+    t.text "ingredients"
+    t.string "bottle_size"
   end
 
   create_table "users", force: :cascade do |t|
