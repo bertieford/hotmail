ff3698509ce405b65152306af3051541d7b4c7ae
Robert Ford <robertford88@gmail.com>
2022-06-07 14:26:20 +0100

    product model complete with validations and references, then migrated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,43 @@
 class ProductsController < ApplicationController
 
   def new
-
+    @product = Product.new
   end
 
   def create
-
+    @product = Product.new(product_params)
+    if @product.save
+      redirct_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
-
+    @product = Product.find(params[:id])
   end
 
   def index
-
+    @products = Product.all
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
-
+    @product = product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, status: :see_other
   end
 
   private
