f106337bf7a3cf1acb5f237cb74eec4c8b397d31
sam neat <samjneat@gmail.com>
2022-06-09 14:47:01 +0100

    reviews can be made and show on the product show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,10 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    # @review.user = current_user
+    @review.user = current_user
     @review.product = @product
     if @review.save
-      render product_path(@product)
+      redirect_to product_path(@product)
     else
       render :new, status: :unprocessable_entity
     end
