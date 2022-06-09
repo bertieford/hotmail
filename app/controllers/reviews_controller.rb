4b4adddd6d1d78a117f4939295b225bb93ad4b4f
sam neat <samjneat@gmail.com>
2022-06-09 11:26:53 +0100

    added create action in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :set_product, only: [:create]
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if review.save
+      render product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
 end
