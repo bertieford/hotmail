2955967abc1eab2a3554a4fae2f73da4e09036c8
sam neat <samjneat@gmail.com>
2022-06-15 12:17:16 +0100

    added actual cards to the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home, :blog]
 
+  def home
+    @gift_products = Product.last(4)
+    @rand_products = Product.all.sample(4)
+
+
+
+  end
+
   def blog
 
   end
