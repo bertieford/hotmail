dd9053f2c8f5a02cd99d57cd590a7b7d495d0493
sam neat <samjneat@gmail.com>
2022-06-15 11:40:03 +0100

    added the gift sets

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   # validates :ingredients, presence: true
-  validates :bottle_size, presence: true
+  # validates :bottle_size, presence: true
   validates :manufacturer, presence: true
   validates :price, presence: true
   has_one_attached :photo
