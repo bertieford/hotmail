bd624cc8dd9bba79ef15e3ec8e58627090767333
Robert Ford <robertford88@gmail.com>
2022-06-16 10:32:11 +0100

    amended migration naming

--- db/migrate/20220608101520_add_price_to_order.rb
+++ db/migrate/20220608101520_add_price_to_order.rb
@@ -1,4 +1,4 @@
-class AddCompleteToOrder < ActiveRecord::Migration[7.0]
+class AddPriceToOrder < ActiveRecord::Migration[7.0]
   def change
     add_column :orders, :price, :float, :default => 0
   end
