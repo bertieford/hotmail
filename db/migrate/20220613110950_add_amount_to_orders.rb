439c85d00ebfc6271bf40cb4edd12ca4cb3823c2
Robert Ford <robertford88@gmail.com>
2022-06-16 09:52:42 +0100

    bug migration heroku fix

--- db/migrate/20220613110950_add_amount_to_orders.rb
+++ db/migrate/20220613110950_add_amount_to_orders.rb
@@ -1,5 +1,6 @@
 class AddAmountToOrders < ActiveRecord::Migration[7.0]
   def change
     add_monetize :orders, :amount, curerncy: { present: false }
+    add_column :orders, :state, :string, default: 'pending'
   end
 end
--- db/migrate/20220613140556_add_state_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddStateToOrders < ActiveRecord::Migration[7.0]
-  def change
-    add_column :orders, :state, :string, default: 'pending'
-  end
-end
